4359c0c025e900803cbf398806f02a0c7aaee76d
Swastik Mishra <coolswastik60@gmail.com>
2025-02-04 01:19:06 +0800

    Initial commit

--- api/api/serverless.ts
+++ api/api/serverless.ts
@@ -1,6 +1,11 @@
+import { FastifyRequest } from "fastify/types/request";
 import app from "../app";
+import { FastifyReply } from "fastify/types/reply";
 
-export default async function handler(req, reply) {
+export default async function handler(
+    request: FastifyRequest,
+    reply: FastifyReply
+) {
     await app.ready();
-    app.server.emit("request", req, reply);
+    app.server.emit("request", request, reply);
 }
